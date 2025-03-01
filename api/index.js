bbb06f98d668d081dd189d4a8fe547f4bbdf5844
rezwanahamed <rezwanahamed81@gmail.com>
2025-03-01 18:07:52 +0600

    refactor: remove Vercel configuration and serverless handler for API

--- api/index.js
+++
@@ -1,5 +0,0 @@
-const { createServer } = require('@vercel/node');
-const server = require('../bin/www'); // Import your Express server
-
-// Export the serverless handler
-module.exports = createServer(server);
